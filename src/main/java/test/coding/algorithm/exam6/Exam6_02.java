031c9c3173cfc9660f3bf5845e947a1fbc02bf04
ksan <aquam41@gmail.com>
2023-12-04 23:14:25 +0900

    coding test study_sort2

---
+++ src/main/java/test/coding/algorithm/exam6/Exam6_02.java
@@ -0,0 +1,33 @@
+package test.coding.algorithm.exam6;
+
+import java.util.Scanner;
+
+public class Exam6_02 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+        solution(arr);
+        in.close();
+        return;
+    }
+
+    public static void solution(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+}
