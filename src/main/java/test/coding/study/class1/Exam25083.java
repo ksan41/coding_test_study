3a300a0b4bb21309c85badf4547379f7eaa5beba
ksan <aquam41@gmail.com>
2024-05-24 16:10:46 +0900

    백준 25083, 27866

---
+++ src/main/java/test/coding/study/class1/Exam25083.java
@@ -0,0 +1,16 @@
+package test.coding.study.class1;
+
+public class Exam25083 {
+    
+    // 새싹
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer();
+        sb.append("         ,r'\"7\n");
+        sb.append("r`-_   ,'  ,/\n");
+        sb.append(" \\. \". L_r\'\n");
+        sb.append("   `~\\/\n");
+        sb.append("      |\n");
+        sb.append("      |");
+        System.out.println(sb.toString());
+    }
+}
---
+++ src/main/java/test/coding/study/class1/Exam27866.java
@@ -0,0 +1,16 @@
+package test.coding.study.class1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Exam27866 {
+    
+    // 문자와 문자열
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int i = Integer.parseInt(br.readLine());
+        System.out.println(str.charAt(i - 1));
+    }
+}
