4e057e75712cfcbb9e978179148785ff2c51c840
ksan <aquam41@gmail.com>
2024-06-07 09:50:19 +0900

    백준 11286

---
+++ src/main/java/test/coding/study/class3/Exam11286.java
@@ -0,0 +1,58 @@
+package test.coding.study.class3;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Exam11286 {
+    
+    static int n = 0;
+
+    // 절댓값 힙
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n =  Integer.parseInt(st.nextToken());
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                if (Math.abs(o1) > Math.abs(o2)) {
+                    return 1;
+                } else if (Math.abs(o1) < Math.abs(o2)) {
+                    return -1;
+                } else {
+                    if (o1 > o2) {
+                        return 1;
+                    } else {
+                        return -1;
+                    }
+                }
+            }
+            
+        });
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x =  Integer.parseInt(st.nextToken());
+            if (x != 0) {
+                queue.offer(x);
+            } else {
+                if (!queue.isEmpty()) {
+                    bw.append(queue.poll() + "");
+                } else {
+                    bw.append(0 +"");
+                }
+
+                bw.newLine();
+            }
+        }
+        bw.flush();
+    }
+}
